012190eaf747c9a8f698bbed61ed9a7e3b4c253d
leorvergani <vergani@outlook.com.br>
2024-09-12 22:54:38 -0300

    finalizado

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, Image, StatusBar, TouchableOpacity, Vibration, ScrollView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import { posts } from './posts'
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <StatusBar backgroundColor="#991c00" />
+
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require('./assets/icon.webp')}
+        />
+        <MaterialCommunityIcons name='chat-outline' size={30} color="#CCC" />
+      </View>
+
+      <ScrollView>
+        {
+          posts.map((post) => (
+            <View key={post.id}>
+
+              <View style={styles.headerPost}>
+                <Text style={styles.whiteColor}>{post.user}</Text>
+                <MaterialCommunityIcons name='dots-horizontal' size={30} color="#ffffff" />
+              </View>
+
+              <Image style={styles.imagePost} source={{ uri: post.image }} />
+
+              <View style={styles.footerPost}>
+                <TouchableOpacity onPress={() => Vibration.vibrate(500)}>
+                  <MaterialCommunityIcons name='heart-outline' size={30} color="#CCC" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <MaterialCommunityIcons name='chat-outline' size={30} color="#CCC" />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <MaterialCommunityIcons name='share-outline' size={30} color="#CCC" />
+                </TouchableOpacity>
+              </View>
+
+              <Text style={styles.likeCount}> {post.likes} likes</Text>
+              <Text style={styles.comment}>{post.caption}</Text>
+            </View>
+          ))
+        }
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safe: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1d1d1d'
   },
-});
+  logo: {
+    width: 40,
+    height: 40
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    alignItems: 'center',
+    borderBottomColor: '#adadad',
+    borderBottomWidth: 1
+  },
+  headerPost: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    alignItems: 'center',
+  },
+  imagePost: {
+    width: '100%', 
+    height: 350, 
+    borderRadius: 10, 
+    resizeMode: 'cover'
+  },
+  footerPost: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: 30
+  },
+  likeCount: {
+    fontWeight: 'bold',
+    marginLeft: 5,
+    color: '#fff'
+  },
+  comment: {
+    textAlign: 'justify',
+    marginLeft: 5,
+    color: '#fff'
+  },
+  whiteColor: {
+    color: '#fff'
+  }
+})
---
+++ posts.js
@@ -0,0 +1,45 @@
+export const posts = [
+    {
+        id: '7',
+        user: 'NaoSouZelda',
+        image: 'https://gartic.com.br/imgs/mural/co/coguperoni/link-zelda-ocarina-of-time.png',
+        likes: 210,
+        caption: 'Olhem minha ocarina.',
+    },
+    {
+        id: '1',
+        user: 'user1',
+        image: 'https://cafeviagem.com/wp-content/uploads/2019/11/Casa-Angelina-Hotel-Costa-Amalfitana-68-960x528.jpg',
+        likes: 120,
+        caption: 'Nada como viajar 12 horas de avião para apreciar um café igual ao de casa.',
+    },
+    {
+        id: '2',
+        user: 'user2',
+        image: 'https://thumbs.dreamstime.com/b/m%C3%A3o-da-mulher-usando-foto-do-telefone-celular-natureza-da-montanha-89745116.jpg',
+        likes: 250,
+        caption: 'A natureza é linda, especialmente quando você tem sinal de Wi-Fi.',
+    },
+
+    {
+        id: '4',
+        user: 'user4',
+        image: 'https://blog.cobasi.com.br/wp-content/webpc-passthru.php?src=https://blog.cobasi.com.br/wp-content/uploads/2022/12/cachorro-dormir-na-cama-capa.png&nocache=1',
+        likes: 350,
+        caption: 'A melhor parte da viagem? A volta, quando posso dormir na minha cama.',
+    },
+    {
+        id: '5',
+        user: 'user5',
+        image: 'https://viajandonajanela.com/wp-content/uploads/2020/05/camera-viagem-equipamento-fotografico.jpg',
+        likes: 145,
+        caption: 'Gastando todas as minhas economias para tirar uma foto perfeita. Valeu a pena?',
+    },
+    {
+        id: '8',
+        user: 'user8',
+        image: 'https://static.vecteezy.com/ti/fotos-gratis/t1/13951373-incrivel-paisagem-natural-bela-cena-com-picos-de-alta-montanha-tatra-pedras-no-lago-de-montanha-agua-calma-do-lago-reflexao-ceu-colorido-por-do-sol-fundo-de-natureza-incrivel-caminhadas-de-aventura-de-outono-foto.jpg',
+        likes: 330,
+        caption: 'Explorando o mundo... desde que haja ar-condicionado.',
+    },
+]
